058d07cf96b5b5cb3ddd2735b0a03e02ebd30375
bashyauri <80903696+bashyauri@users.noreply.github.com>
2022-07-31 12:54:57 +0100

    created composition-api

---
+++ composition-blog/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click Me</button>
+  </div>
+
+  <router-view />
+</template>
+<script>
+export default {
+  setup() {
+    console.log('setup');
+    let name = 'mario'
+    let age = 30
+    const handleClick = () => {
+      console.log('You clicked me')
+    }
+    return {
+      name,
+      age,
+      handleClick,
+
+    }
+  },
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
