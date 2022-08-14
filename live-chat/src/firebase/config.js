9188c72e507e0ba736b477e1313f73f3a31bb26f
bashyauri <80903696+bashyauri@users.noreply.github.com>
2022-08-14 09:41:00 +0100

    Configure firebase
    configure and integrate firebase

---
+++ live-chat/src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyBjxw9GnmEkDKoxRb2I0dVUc4FiA2nE530",
+  authDomain: "my-vue-firebase-site-dce6f.firebaseapp.com",
+  projectId: "my-vue-firebase-site-dce6f",
+  storageBucket: "my-vue-firebase-site-dce6f.appspot.com",
+  messagingSenderId: "1022745557423",
+  appId: "1:1022745557423:web:5552e921bbbcafd912175a",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timeStamp = firebase.firestore.fieldValue.serverTimestamp;
+
+export { projectFirestore, timeStamp };
